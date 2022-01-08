2577ddf0e21cb12f77e1696b02ff16ce5a014c42
Clayton Sulby <claytonsulby@gmail.com>
2022-01-08 17:48:16 +0000

    created install script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root (sudo)"
+  exit 1
+fi
+
+if [[ $1 != "" ]] ; then
+  BRANCH=$1
+else
+  BRANCH="master"
+fi
+
+# Update git
+echo "Updating from git.."
+git reset --hard HEAD
+git pull
+git checkout $BRANCH
+
+# Set permissions on the initial required
+chmod +x install.sh
+
+#Update Mo game
+#echo "Updating child repos from git"
+
+# Move pulled files to correct locations
+echo "Moving files to correct locations"
+#cp -auv 
